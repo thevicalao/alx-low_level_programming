4f43cb9cdfe9c919ab9cb4d72fe2a5fdc2d980c0
Victoria Alao <thevicalao@gmail.com>
2022-12-13 00:44:58 +0100

    Write a program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			for (k = 48; k <= 57; k++)
 			{
-																			for (j = 48; j <= 57; j++)
+				for (j = 48; j <= 57; j++)
 				{
 				if (((k + j) > (c + i) &&  k >= c) || c < k)
 				{
