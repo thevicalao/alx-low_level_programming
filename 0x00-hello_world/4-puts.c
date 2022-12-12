4675b86a471f1eac1b1597556626fbf3080aced1
Victoria Alao <thevicalao@gmail.com>
2022-12-12 22:40:14 +0100

    error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
