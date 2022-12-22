846e6f6431101d5eca4079631bf64bd9846ef847
Victoria Alao <thevicalao@gmail.com>
2022-12-22 15:30:52 +0100

    Write a function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,4 @@
-#include "holberton.h"
-
-/**
- * leet - Encodes a string to 1337.
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
- */
-char *leet(char *str)
-{
-	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-
-	while (str[indx1])
-	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
-		{
-			if (str[indx1] == leet[indx2] ||
-			    str[indx1] - 32 == leet[indx2])
-				str[indx1] = indx2 + '0';
-		}
-
-		indx1++;
-	}
-
-	return (str);
-}
+[master 8a8a939] Write a function that encodes a string into 1337.
+ 2 files changed, 29 insertions(+), 38 deletions(-)
+ rewrite 0x06-pointers_arrays_strings/6-cap_string.c (100%)
+ create mode 100644 0x06-pointers_arrays_strings/7-leet.c
