421cff90eac08e21c1cc9f08063711c3ccbf8d76
Victoria Alao <thevicalao@gmail.com>
2022-12-22 15:13:06 +0100

    Write a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,3 @@
-#include "holberton.h"
-
-/**
- * string_toupper - Changes all lowercase letters
- *                  of a string to uppercase.
- * @str: The string to be changed.
- *
- * Return: A pointer to the changed string.
- */
-char *string_toupper(char *str)
-{
-	int index = 0;
-
-	while (str[index])
-	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
-
-		index++;
-	}
-
-	return (str);
-}
+[master bd3c553] Write a function that changes all lowercase letters of a string to uppercase.
+ 2 files changed, 27 insertions(+), 19 deletions(-)
+ create mode 100644 0x06-pointers_arrays_strings/5-string_toupper.c
